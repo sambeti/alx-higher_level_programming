d1ec41ff94a31fa40ece2091195b443d6db493e0
Samuel Karmole <sambeti2002@gmail.com>
2023-08-10 05:25:44 -0700

    Test file for addition functions

---
+++ 0x02-python-import_modules/add_0.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def add(a, b):
+    """May addition function to be imported to others
+
+        args:
+            a: the first integer
+            b: the second integer
+
+        returns:
+            the return value. a + b
+            """
+        return (a + b)
